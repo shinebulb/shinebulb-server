687f0f7fc005b367971f1650b0a7888ec3f02aa7
Andrew <aldortheold@gmail.com>
2024-12-27 12:46:24 +1000

    got environment variables to work

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    development: {
+      username: `${process.env.DEV_USERNAME}`,
+      password: `${process.env.DEV_PASSWORD}`,
+      database: `${process.env.DEV_DATABASE}`,
+      host: `${process.env.DEV_HOST}`,
+      dialect: "mysql"
+    },
+    test: {
+      username: "root",
+      password: null,
+      database: "database_test",
+      host: "127.0.0.1",
+      dialect: "mysql"
+    },
+    production: {
+      username: `${process.env.PRODUCTION_USERNAME}`,
+      password: `${process.env.PRODUCTION_PASSWORD}`,
+      database: `${process.env.PRODUCTION_DATABASE}`,
+      host: `${process.env.PRODUCTION_HOST}`,
+      dialect: "mysql"
+    }
+}
