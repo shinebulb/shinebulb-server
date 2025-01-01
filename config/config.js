968c65ecdd354e5162e0108b10dd13577f376642
Andrew <aldortheold@gmail.com>
2025-01-01 22:45:33 +1000

    improved syntax

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
-      username: `${process.env.DEV_USERNAME}`,
-      password: `${process.env.DEV_PASSWORD}`,
-      database: `${process.env.DEV_DATABASE}`,
-      host: `${process.env.DEV_HOST}`,
+      username: process.env.DEV_USERNAME,
+      password: process.env.DEV_PASSWORD,
+      database: process.env.DEV_DATABASE,
+      host: process.env.DEV_HOST,
       dialect: "mysql"
     },
     test: {
@@ -14,10 +14,10 @@ module.exports = {
       dialect: "mysql"
     },
     production: {
-      username: `${process.env.PRODUCTION_USERNAME}`,
-      password: `${process.env.PRODUCTION_PASSWORD}`,
-      database: `${process.env.PRODUCTION_DATABASE}`,
-      host: `${process.env.PRODUCTION_HOST}`,
+      username: process.env.PRODUCTION_USERNAME,
+      password: process.env.PRODUCTION_PASSWORD,
+      database: process.env.PRODUCTION_DATABASE,
+      host: process.env.PRODUCTION_HOST,
       dialect: "mysql"
     }
 }
